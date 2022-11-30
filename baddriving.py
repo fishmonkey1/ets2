a8d9eee372f2ee3a91ba0d61d170d48d3dcf45d9
GageBrk <GageBrk@gmail.com>
2022-11-30 12:09:24 -0500

    No solution for masking/LSD issue. pre-commit before attempting to disqualify lines on ROI mask.

--- baddriving.py
+++ baddriving.py
@@ -26,36 +26,46 @@ vertices = {"road_vertices":     [np.array([[300,508], [340,508], [390,475], [46
             "l_mirror_vertices": [np.array([[18,285], [169,285], [169,362], [18,362]], dtype=np.int32)],
             "r_mirror_vertices": [np.array([[852,285], [1003,285], [1003,362], [852,362]], dtype=np.int32)]}
 
+# Going to just ignore lines with slopes that are the same as slopes of vertices....
+ignored_slopes = None
+
 def roi(image,vertices):
     # revisit later, might not be nessecary? trying to set no color for masking
-    # channel_count = image.shape[0]  # i.e. 3 or 4 depending on your image?
+    # channel_count = image.shape[2]  # i.e. 3 or 4 depending on your image?
     # color = (255,)*channel_count #this makes the mask clear
 
     color = 255
+    
 
     # TODO: Refactor:
     #       loop and put masks into dict
     #       return single dict
 
-    mask = np.zeros_like(image) # empty array of zeros (empty pixels) in same shape as frame
+    mask = np.zeros_like(image,dtype="uint8" ) # empty array of zeros (empty pixels) in same shape as frame
     cv2.fillPoly(mask, vertices["road_vertices"], color) # create polygon shape (mask)
     masked_road = cv2.bitwise_and(image, mask) # show only area of original image that is mask shape
 
-    mask = np.zeros_like(image)
+    mask = np.zeros_like(image, dtype="uint8")
     cv2.fillPoly(mask, vertices["l_mirror_vertices"], color)
     masked_L_mirror = cv2.bitwise_and(image, mask)
 
-    mask = np.zeros_like(image)
+    mask = np.zeros_like(image, dtype="uint8")
     cv2.fillPoly(mask, vertices["r_mirror_vertices"], color)
     masked_R_mirror = cv2.bitwise_and(image, mask)
     
-    mask = np.zeros_like(image)
+    mask = np.zeros_like(image, dtype="uint8")
     cv2.fillPoly(mask, vertices["road_vertices"], color)
     cv2.fillPoly(mask, vertices["l_mirror_vertices"], color)
     cv2.fillPoly(mask, vertices["r_mirror_vertices"], color)
     total_masks = cv2.bitwise_and(image, mask)
 
     return masked_road, masked_L_mirror, masked_R_mirror, total_masks
+
+def is_ROI_boundary(xyxy):
+    '''
+    Returns True if line is on ROI.
+    '''
+    pass
 
 def get_slopes(lines, include_horizontal=False):
     '''
@@ -72,9 +82,15 @@ def get_slopes(lines, include_horizontal=False):
     for idx,i in enumerate(lines):
         for xyxy in i:
 
+            if is_ROI_boundary():
+                continue # Go to next set of coords
+
             # These four lines:
             # modified from http://stackoverflow.com/questions/21565994/method-to-return-the-equation-of-a-straight-line-given-two-points
             # Used to calculate the definition of a line, given two sets of coords.
+
+            #LSD returns floats not ints for some reason...need to round
+
             x_coords = (xyxy[0],xyxy[2])
             y_coords = (xyxy[1],xyxy[3])
             A = vstack([x_coords,ones(len(x_coords))]).T
@@ -103,14 +119,21 @@ def get_slopes(lines, include_horizontal=False):
             if m < 0:
                 line_dict['neg'][idx] = [m,b,[xyxy[0], xyxy[1], xyxy[2], xyxy[3]]]
                 added +=1
-      
+    
+    #pp.pprint(line_dict)  
     return line_dict 
 
 
 
-def draw_lines(image, coords, color):
+def draw_lines(image, coords, color, LSD=None):
              #coords = line[0]
-    cv2.line(image, (coords[0], coords[1]), (coords[2], coords[3]), color, 2)
+    if LSD is not None:
+        for xyxy in LSD:
+            
+            #x0, y0, x1, y1 = line.flatten()
+            cv2.line(image, (int(xyxy[0][0]), int(xyxy[0][1])), (int(xyxy[0][2]),int(xyxy[0][3])), color, 2)
+    else:        
+        cv2.line(image, (coords[0], coords[1]), (coords[2], coords[3]), color, 2)
     
 
 def draw_vertices(frame, vertices):
@@ -129,7 +152,13 @@ def draw_vertices(frame, vertices):
         
     #cv2.imwrite("pics/testing/vertices.jpg", frame)
 
-def process_frame(frame):
+def preprocess():
+    # Mask larger polygon,
+    # Then canny edge detect
+    # Then mask actual size again to get rid of border...
+    pass
+
+def process_frame(frame, detectionType):
     '''
     https://stackoverflow.com/questions/45127421/when-applying-the-canny-function-can-you-apply-the-mask-first
     Doing masking before edge detection results in detecting edge border around mask.
@@ -143,61 +172,95 @@ def process_frame(frame):
     '''
     
     #gets full frame ready for edge detection
+    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    canny = cv2.Canny(gray, threshold1=200, threshold2=300) #200/300
-    canny_blurred = cv2.GaussianBlur(canny, (5,5), 0)
+    if detectionType == 'Hough':
+        canny = cv2.Canny(gray, threshold1=200, threshold2=300) #200/300
+        canny_blurred = cv2.GaussianBlur(canny, (5,5), 0)
 
-    #this draws mask poly on pic
-    draw_vertices(frame,vertices)
+        #this draws mask poly on pic
+        draw_vertices(frame,vertices)
 
-    masked_road, masked_l_mirror, masked_r_mirror, total_masks = roi(canny_blurred, vertices)
-    
-    # this saves an image on exit
-    #cv2.imwrite("pics/testing/total_masks.jpg", total_masks)
+        masked_road, masked_l_mirror, masked_r_mirror, total_masks = roi(canny_blurred, vertices)
+        
+        # this saves an image on exit
+        #cv2.imwrite("pics/testing/total_masks.jpg", total_masks)
+        
+        road_lines = cv2.HoughLinesP(masked_road, cv2.HOUGH_PROBABILISTIC, 1*np.pi/180, 200, np.array([]), 40, 10)
+        l_mirror_lines = cv2.HoughLinesP(masked_l_mirror, cv2.HOUGH_PROBABILISTIC, 1*np.pi/180, 200, np.array([]), 2, 5)
+        r_mirror_lines = cv2.HoughLinesP(masked_r_mirror, cv2.HOUGH_PROBABILISTIC, 1*np.pi/180, 200, np.array([]), 2, 5)
 
-    road_lines = cv2.HoughLinesP(masked_road, cv2.HOUGH_PROBABILISTIC, 1*np.pi/180, 200, np.array([]), 40, 10)
-    l_mirror_lines = cv2.HoughLinesP(masked_l_mirror, cv2.HOUGH_PROBABILISTIC, 1*np.pi/180, 200, np.array([]), 2, 5)
-    r_mirror_lines = cv2.HoughLinesP(masked_r_mirror, cv2.HOUGH_PROBABILISTIC, 1*np.pi/180, 200, np.array([]), 2, 5)
-    
-
-    if l_mirror_lines is not None:
-        print(len(l_mirror_lines))
-    if r_mirror_lines is not None:
-        print(len(r_mirror_lines))
     
 
-    all_slopes = []
-    if road_lines is not None:
-        road_slopes = get_slopes(road_lines, True)
-        all_slopes.append(road_slopes)
-    if l_mirror_lines is not None:
-        l_mirror_slopes = get_slopes(l_mirror_lines, True)
-        all_slopes.append(l_mirror_slopes)
-    if r_mirror_lines is not None:
-        r_mirror_slopes = get_slopes(r_mirror_lines, True)
-        all_slopes.append(r_mirror_slopes)
+
+    # if l_mirror_lines is not None:
+    #     print(len(l_mirror_lines))
+    # if r_mirror_lines is not None:
+    #     print(len(r_mirror_lines))
+    
+
+        all_slopes = []
+        if road_lines is not None:
+            road_slopes = get_slopes(road_lines, True)
+            all_slopes.append(road_slopes)
+        if l_mirror_lines is not None:
+            l_mirror_slopes = get_slopes(l_mirror_lines, True)
+            all_slopes.append(l_mirror_slopes)
+        if r_mirror_lines is not None:
+            r_mirror_slopes = get_slopes(r_mirror_lines, True)
+            all_slopes.append(r_mirror_slopes)
 
 
-    for slopes in all_slopes:
-        for pos_neg_horiz in slopes.keys():
-            
-            # Sets color of line for drawing
-            if pos_neg_horiz == 'pos':
-                color = [255,0,0] # Green
-            if pos_neg_horiz == 'neg':
-                color = [0,255,0] # Blue
-            if pos_neg_horiz == 'horizontal':
-                color = [0,0,255] # Red
+        for slopes in all_slopes:
+            for pos_neg_horiz in slopes.keys():
+                
+                # Sets color of line for drawing
+                if pos_neg_horiz == 'pos':
+                    color = [255,0,0] # Green
+                if pos_neg_horiz == 'neg':
+                    color = [0,255,0] # Blue
+                if pos_neg_horiz == 'horizontal':
+                    color = [0,0,255] # Red
 
-            # Draws lines on frame
-            for idx in slopes[pos_neg_horiz]:
-                draw_lines(frame, slopes[pos_neg_horiz][idx][2], color)
+                # Draws lines on frame
+                for idx in slopes[pos_neg_horiz]:
+                    draw_lines(frame, slopes[pos_neg_horiz][idx][2], color)
+
+    if detectionType == 'LSD':
+        lsd = cv2.createLineSegmentDetector(cv2.LSD_REFINE_STD)          
+                                                                #need to pass in grayscale
+        masked_road, masked_l_mirror, masked_r_mirror, total_masks = roi(gray, vertices)
+
+        # masked_road = cv2.cvtColor(masked_road, cv2.COLOR_BGR2GRAY)
+        # masked_l_mirror = cv2.cvtColor(masked_l_mirror, cv2.COLOR_BGR2GRAY)
+        # masked_r_mirror = cv2.cvtColor(masked_r_mirror, cv2.COLOR_BGR2GRAY)
+        # total_masks = cv2.cvtColor(total_masks, cv2.COLOR_BGR2GRAY)
+
+        # this saves an image on exit
+        cv2.imwrite("pics/testing/total_masks.jpg", total_masks)
+
+        road_lines = lsd.detect(masked_road)[0]
+        l_mirror_lines = lsd.detect(masked_l_mirror)[0]
+        r_mirror_lines = lsd.detect(masked_r_mirror)[0]
+        total_lines = lsd.detect(total_masks)[0]
+
+        # lsd.drawSegments(frame,road_lines)
+        # lsd.drawSegments(frame,l_mirror_lines)
+        # lsd.drawSegments(frame,r_mirror_lines)
+
+        # draw_lines(frame, coords=None, color=[255,0,0], LSD=road_lines)
+        # draw_lines(frame, coords=None, color=[255,0,0], LSD=l_mirror_lines)
+        # draw_lines(frame, coords=None, color=[255,0,0], LSD=r_mirror_lines)
+
+        draw_lines(frame, coords=None, color=[255,0,0], LSD=total_lines)
+
+
                 
     return frame
 
 #runtime = "GAME" # don't think this is working quite yet
-runtime = "VIDEO"
-def main(runtime):
+#runtime = "VIDEO"
+def main(runtime, lineDetectionType):
     if runtime == "GAME":
         
         # just a countdown so you can click on game and bring to focus
@@ -228,6 +291,7 @@ def main(runtime):
         frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
         paused = False
 
+        cv2.LineSegmentDetector()
 
         while video.isOpened():
             ret, screen = video.read()
@@ -236,7 +300,8 @@ def main(runtime):
                 print("Can't find frame. Exiting")
                 break  
                 
-            screen_with_lines = process_frame(screen)
+            screen_with_lines = process_frame(screen, lineDetectionType)
+            #screen_with_lines = process_frame(screen, detectionType='Hough')
             screen_with_lines = cv2.resize(screen_with_lines, (896,500))
 
             # uncomment if video output wanted
@@ -286,11 +351,19 @@ def main(runtime):
                     break
                 if key == ord('p'): #press p to pause
                     paused = True
-                    
+        video.release()
+        cv2.destroyAllWindows()            
+
+    if runtime == "PICTURE":
+        path = 'pics/cabin1.png'
+        frame = cv2.imread(path)
+        screen_with_lines = process_frame(frame, lineDetectionType)
+
+        cv2.imwrite("pics/testing/output.jpg", screen_with_lines)
+            
 
     # uncomment if video output wanted
-    video.release()
-    cv2.destroyAllWindows()
+    
 
 
 #####################################
@@ -303,10 +376,18 @@ TODO:
               which can size/position each window so that we can
               nicely display multiple videos at once without overlapping.
 
-        - 
+        - "Line Segment Detection (LSD)" vs. HoughLinesP for line detection...
+          https://www.researchgate.net/figure/Comparison-of-LSD-and-HoughLines-a-and-d-are-the-sample-images-b-e-result-of_fig2_322005775
+          
+          https://docs.opencv.org/3.4/db/d73/classcv_1_1LineSegmentDetector.html#a1816a3c27f7c9b8d8acffec14451d4c4
+          https://stackoverflow.com/questions/41329665/linesegmentdetector-in-opencv-3-with-python
 '''
 show_frame_with_lines = True
 show_canny = False
 
+runtime = 'PICTURE' 
+runtime = 'VIDEO'         #  Can be 'VIDEO' or 'GAME' or "PICTURE"
+lineDetectionType = 'LSD'  #  Can be 'LSD' or 'Hough'
+#lineDetectionType = 'Hough'
 
-main(runtime = 'VIDEO')
+main(runtime, lineDetectionType)
